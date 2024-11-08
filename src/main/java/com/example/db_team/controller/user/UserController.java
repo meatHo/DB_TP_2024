24db1a88399f395e271ac6a7652253e012bbb880
john0609 <nihaojohn@naver.com>
2024-11-08 19:03:18 +0900

    Swagger 설정 추가

--- src/main/java/com/example/db_team/controller/user/UserController.java
+++ src/main/java/com/example/db_team/controller/user/UserController.java
@@ -1,10 +1,10 @@
-package com.example.db_team.controller;
+package com.example.db_team.controller.user;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class UserController {
+public class UserController implements UserControllerDocs {
 
     @GetMapping("/")
     public String HelloWorld() {
---
+++ src/main/java/com/example/db_team/controller/user/UserControllerDocs.java
@@ -0,0 +1,8 @@
+package com.example.db_team.controller.user;
+
+import io.swagger.v3.oas.annotations.tags.Tag;
+
+@Tag(name = "User", description = "사용자 관리 API")
+public interface UserControllerDocs {
+
+}
