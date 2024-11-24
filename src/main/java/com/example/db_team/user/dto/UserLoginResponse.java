787c3124f708d686e70b84a0416c3ac30651eb3e
2ju0123 <2ju1230@gmail.com>
2024-11-25 05:39:15 +0900

    C:/Program Files/Git/api/reviews/me 백엔드 추가

--- src/main/java/com/example/db_team/user/dto/UserLoginResponse.java
+++ src/main/java/com/example/db_team/user/dto/UserLoginResponse.java
@@ -3,6 +3,6 @@ package com.example.db_team.user.dto;
 import io.swagger.v3.oas.annotations.media.Schema;
 
 public record UserLoginResponse(
-        @Schema(description = "유저 ID", example = "1")
+        @Schema(description = "유저 식별 ID", example = "1")
         Long userId) {
 }
