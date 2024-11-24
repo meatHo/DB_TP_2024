80c1605ca036eeb33f0a3554ddfa2715ab0560dc
2ju0123 <2ju1230@gmail.com>
2024-11-24 15:53:09 +0900

    SessionController 추가

---
+++ src/main/java/com/example/db_team/controller/SessionController.java
@@ -0,0 +1,22 @@
+package com.example.db_team.controller;
+
+import jakarta.servlet.http.HttpSession;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/api")
+public class SessionController {
+
+    @GetMapping("/check_login")
+    public ResponseEntity<Void> checkLoginStatus(HttpSession session) {
+        if (session.getAttribute("userId") != null) {
+            return ResponseEntity.ok().build(); // 세션에 userId가 있으면 200 OK
+        } else {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build(); // 로그인되지 않은 경우 401
+        }
+    }
+}
