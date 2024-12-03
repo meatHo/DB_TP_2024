8a46522374d0ce2677e90ec2874d8781e3fdd4db
2ju0123 <2ju1230@gmail.com>
2024-12-03 14:42:32 +0900

    db 구축 완료

--- src/main/java/com/example/db_team/user/domain/User.java
+++ src/main/java/com/example/db_team/user/domain/User.java
@@ -12,7 +12,7 @@ public class User {
 
     @Id
     @Column(name = "user_id")
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long userId;
     @Column(name = "login_id", unique = true, nullable = false)
     private String loginId;
